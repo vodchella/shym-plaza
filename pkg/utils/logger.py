cfc6a022aa7b0416225673aa755dd96d6c847454
vodchella <twister.kz@gmail.com>
2020-06-29 00:19:53 +0600

    LOG_CONFIG moved to pkg.config.logger

--- pkg/utils/logger.py
+++ pkg/utils/logger.py
@@ -1,51 +1,11 @@
 import logging.config
 import logging.handlers
-import os
-import sys
-from pkg.config import CONFIG
-from pkg.constants.date_formats import DATE_FORMAT_FULL
+from pkg.config.logger import LOG_CONFIG
 from pkg.constants.logger import *
-
-LOG_CONFIG = {
-    'version': 1,
-    'formatters': {
-        'simple': {
-            'format': LOGGING_FORMAT,
-            'datefmt': DATE_FORMAT_FULL
-        }
-    },
-    'handlers': {
-        'internal': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple',
-            'stream': sys.stderr
-        },
-        'timedRotatingFile': {
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'formatter': 'simple',
-            'when': 'midnight',
-            'filename': os.path.join(CONFIG['log']['path'],
-                                     CONFIG['log']['file_name'])
-        },
-    },
-    'loggers': {
-        DEFAULT_LOGGER_NAME: {
-            'level': 'DEBUG',
-            'handlers': ['internal', 'timedRotatingFile']
-        },
-        FTP_LOGGER_NAME: {
-            'level': 'DEBUG',
-            'handlers': ['internal', 'timedRotatingFile']
-        },
-        UMAG_LOGGER_NAME: {
-            'level': 'DEBUG',
-            'handlers': ['internal', 'timedRotatingFile']
-        },
-    }
-}
 
 
 logging.config.dictConfig(LOG_CONFIG)
+
 DEFAULT_LOGGER = logging.getLogger(DEFAULT_LOGGER_NAME)
 FTP_LOGGER = logging.getLogger(FTP_LOGGER_NAME)
 UMAG_LOGGER = logging.getLogger(UMAG_LOGGER_NAME)
